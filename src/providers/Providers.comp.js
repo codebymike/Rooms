3947ccd9bc96b9f6377d591795e5232575f585a1
Mike Thomas <mikethomas84@gmail.com>
2022-05-11 18:35:59 +0100

    auth provider/hook init

--- src/providers/Providers.comp.js
+++ src/providers/Providers.comp.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import AuthProvider from './AuthProvider'
 
 export default function Providers({ children }) {
   return (
     <BrowserRouter>
-        <div className="app">
-            {children}
-        </div>
+      <AuthProvider>
+      <div className="app">
+              {children}
+          </div>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
