7d6c29229dcd85aa3ee8a67bf8c167bb1fc1b0d4
Mike Thomas <mikethomas84@gmail.com>
2022-05-23 14:53:27 +0100

    fixed wallet, added callback hook

--- src/hooks/usePopmojiCoin.hook.js
+++ src/hooks/usePopmojiCoin.hook.js
@@ -1,4 +1,4 @@
-import { useEffect, useReducer } from 'react'
+import { useEffect, useReducer, useCallback } from 'react'
 import { CREATE_COIN_VAULT } from '../flow/transactions/create-coin-vault.tx';
 import { GET_COIN_BALANCE } from '../flow/scripts/get-coin-balance.script';
 import { defaultReducer } from '../reducer/defaultReducer'
@@ -13,31 +13,34 @@ export default function usePopmojiCoin(user) {
   })
   const { addTx } = useTxs()
 
+
+  const getCoinBalance = useCallback(() => {
+    const callback = async () => {
+        if (!user?.addr) return
+    
+        console.log('address: ', user?.addr,);
+        dispatch({ type: 'PROCESSING' })
+    
+        try {
+          let response = await query({
+            cadence: GET_COIN_BALANCE,
+            args: (arg, t) => [
+              arg(user?.addr, t.Address)
+            ]
+          })
+          dispatch({ type: 'SUCCESS', payload: response })
+        } catch (err) {
+          dispatch({ type: 'ERROR' })
+          console.log(err)
+        }
+      }
+      callback();
+    },[user]
+  )
+
   useEffect(() => {
     getCoinBalance();
-    //eslint-disable-next-line 
-  }, [])
-
-  const getCoinBalance = async () => {
-
-    if (!user?.addr) return
-
-    console.log('address: ', user?.addr,);
-    dispatch({ type: 'PROCESSING' })
-
-    try {
-      let response = await query({
-        cadence: GET_COIN_BALANCE,
-        args: (arg, t) => [
-          arg(user?.addr, t.Address)
-        ]
-      })
-      dispatch({ type: 'SUCCESS', payload: response })
-    } catch (err) {
-      dispatch({ type: 'ERROR' })
-      console.log(err)
-    }
-  }
+  }, [getCoinBalance])
 
   const createCoinVault = async () => {
     dispatch({ type: 'PROCESSING' })
