45636f66d5d96c7772ca80e01a95c44b2875a385
Mike Thomas <mikethomas84@gmail.com>
2022-06-07 13:59:32 +0100

    metadata contract config

--- src/config/config.js
+++ src/config/config.js
@@ -11,4 +11,5 @@ config({
   "0xPopmojiCoin":      process.env.REACT_APP_POPMOJICOIN_CONTRACT,
   "0xPopmojiItem":      process.env.REACT_APP_POPMOJIITEM_CONTRACT,
   "0xNFTMarketplace":   process.env.REACT_APP_NFTMARKETPLACE_CONTRACT,
+  "0xMetaDataViews":    process.env.REACT_APP_METADATAVIEWS_CONTRACT,
 })
