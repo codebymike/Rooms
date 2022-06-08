ab36962b9ffa1c0c6ea3c380789a05a3bffdf74b
Mike Thomas <mikethomas84@gmail.com>
2022-06-08 12:58:07 +0100

    env debug console.log

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import './config/config' // FCL config
 import './index.css';
 
+console.log(`Started on: ${process.env.REACT_APP_ENV}`);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
