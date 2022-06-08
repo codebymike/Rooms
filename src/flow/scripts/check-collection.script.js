51fa38a246db6932f9c97d988d4b382c8b5fe066
Mike Thomas <mikethomas84@gmail.com>
2022-06-08 15:21:26 +0100

    correct collection check storage path

--- src/flow/scripts/check-collection.script.js
+++ src/flow/scripts/check-collection.script.js
@@ -2,7 +2,7 @@ export const CHECK_COLLECTION = `
   import PopmojiItem from 0xPopmojiItem
   
   pub fun main(addr: Address): Bool {
-    let ref = getAccount(addr).getCapability<&{PopmojiItem.CollectionPublic}>(PopmojiItem.CollectionStoragePath).check()
+    let ref = getAccount(addr).getCapability<&{PopmojiItem.PopmojiItemCollectionPublic}>(PopmojiItem.CollectionPublicPath).check()
     return ref
   }
 `
